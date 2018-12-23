bbdb2e14b2ccc35923a4c681e8cafef01ce93b71
Shlomi <vgtvgy1@gmail.com>
2018-12-23 12:06:51 +0200

    Completed home activity

--- ariel-trivia-app/src/main/java/com/example/root/ariel_trivia_app/HomeActivity.java
+++ ariel-trivia-app/src/main/java/com/example/root/ariel_trivia_app/HomeActivity.java
@@ -1,16 +1,26 @@
 package com.example.root.ariel_trivia_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
-public class homeActivity extends Activity {
+public class HomeActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        Button btn = findViewById(R.id.home_btn_next);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
