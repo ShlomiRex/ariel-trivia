5063d88cce212941a89069850c578cbc04640266
amgross <aryemg@gmail.com>
2018-12-25 22:52:21 +0200

    can upload questions!

--- ariel-trivia-app/src/main/java/com/example/root/ariel_trivia_app/addTriviaQuestionActivity.java
+++ ariel-trivia-app/src/main/java/com/example/root/ariel_trivia_app/addTriviaQuestionActivity.java
@@ -8,27 +8,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.root.ariel_trivia_app.base.Trivia;
+
 public class addTriviaQuestionActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trivia_question);
-//        final Button add = (Button) findViewById(R.id.add_question_button);
-//        add.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String question =  ((EditText) findViewById(R.id.myQuestion)).getText().toString();
-//                String answer =  ((EditText) findViewById(R.id.correctAnswer)).getText().toString();
-//                String[] wrongAnswers = new String[3];
-//                wrongAnswers[0] =  ((EditText) findViewById(R.id.myFirstAnswer)).getText().toString();
-//                wrongAnswers[1] =  ((EditText) findViewById(R.id.mySecondAnswer)).getText().toString();
-//                wrongAnswers[2] =  ((EditText) findViewById(R.id.myThirdAnswer)).getText().toString();
-//                APIRequests.uploadTrivia(question, answer, wrongAnswers);
-//                Intent i = new Intent(addTriviaQuestionActivity.this, addTriviaQuestionActivity.class);
-//                finish();
-//                startActivity(i);
-//            }
-//        });
+        final Button add = (Button) findViewById(R.id.add_question_button);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question =  ((EditText) findViewById(R.id.myQuestion)).getText().toString();
+                String answer =  ((EditText) findViewById(R.id.correctAnswer)).getText().toString();
+                String[] wrongAnswers = new String[3];
+                wrongAnswers[0] =  ((EditText) findViewById(R.id.myFirstAnswer)).getText().toString();
+                wrongAnswers[1] =  ((EditText) findViewById(R.id.mySecondAnswer)).getText().toString();
+                wrongAnswers[2] =  ((EditText) findViewById(R.id.myThirdAnswer)).getText().toString();
+                Global.apiRequests.uploadTrivia(new Trivia(question, answer, wrongAnswers));
+                Intent i = new Intent(addTriviaQuestionActivity.this, addTriviaQuestionActivity.class);
+                finish();
+                startActivity(i);
+            }
+        });
     }
 }
