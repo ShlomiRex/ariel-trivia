22cd59500bc1af3cfda9098b95f2a6ed7cd1d3f2
amgross <aryespa@gmail.com>
2018-12-27 15:25:09 +0200

    unable buttons when no need
    unable buttons when no need at - register, login, add comments, add questions

--- ariel-trivia-app/src/main/java/com/example/root/ariel_trivia_app/LoginActivity.java
+++ ariel-trivia-app/src/main/java/com/example/root/ariel_trivia_app/LoginActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +33,35 @@ import java.io.InputStreamReader;
 public class LoginActivity extends Activity {
     private final String TAG = LoginActivity.class.getSimpleName();
     private Nitrite db;
+    private Button btn_login;
+    private EditText etxt_username;
+    private EditText etxt_password;
+
+    private TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            checkFieldsForEmptyValues();
+        }
+    };
+
+    void checkFieldsForEmptyValues(){
+
+
+        if(etxt_username.getText().toString().equals("")||  etxt_password.getText().toString().equals("")){
+            btn_login.setEnabled(false);
+        } else {
+            btn_login.setEnabled(true);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +70,12 @@ public class LoginActivity extends Activity {
         db = Nitrite.builder().filePath(path).openOrCreate();
         //Set global database
         Global.database = db;
-        final Button btn_login = (Button) findViewById(R.id.login_btn_login);
-        final EditText etxt_username = (EditText) findViewById(R.id.login_etxt_username);
-        final EditText etxt_password = (EditText) findViewById(R.id.login_etxt_password);
-
+        btn_login = (Button) findViewById(R.id.login_btn_login);
+        etxt_username = (EditText) findViewById(R.id.login_etxt_username);
+        etxt_password = (EditText) findViewById(R.id.login_etxt_password);
+        etxt_username.addTextChangedListener(mTextWatcher);
+        etxt_password.addTextChangedListener(mTextWatcher);
+        btn_login.setEnabled(false);
         final Button btn_register = findViewById(R.id.login_btn_register);
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
